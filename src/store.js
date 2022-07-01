40b75053ac7bf93cb298357717effd0dfae98327
Jeffrey <jeff.1001@live.com>
2022-07-01 16:04:55 -0500

    acceptance criteria met

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './features/CounterSlice'
+import todoReducer from './features/todoSlice'
+
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+        todo: todoReducer
+    }
+})
